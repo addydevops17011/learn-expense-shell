d568de8cf05bd2dced7ab0df2e806b2687e97ca6
aditya <adityavittal92@gmail.com>
2024-11-07 12:51:01 +0530

    update
    update

--- frontend.sh
+++ frontend.sh
@@ -6,7 +6,7 @@ echo -e "\e[32m configure nginx \e[0m"
 cp /home/ec2-user/learn-expense-shell/expense.conf /etc/nginx/default.d/expense.conf
 
 
-echo -e "\e[32m download the frontend.zip file \e[0m"
+
 
 rm -rf /usr/share/nginx/html/*
 
