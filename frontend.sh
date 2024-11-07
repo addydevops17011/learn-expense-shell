3db57e50169c40ff9df5498981249ba11e8d9f3a
aditya <adityavittal92@gmail.com>
2024-11-07 12:04:42 +0530

    updated
    updated

--- frontend.sh
+++ frontend.sh
@@ -6,4 +6,7 @@ curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/expense-fro
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 #vim /etc/nginx/default.d/expense.conf
+
+cp expense.conf /etc/nginx/default.d/expense.conf
+
 systemctl restart nginx
