bfb8d00517f9509764b608253ba4688689ec211b
aditya <adityavittal92@gmail.com>
2024-11-08 14:20:29 +0530

    update
    update

--- backend.sh
+++ backend.sh
@@ -68,7 +68,7 @@ STAT $?
 
 # Initialize the mysql database with the backend schema
 Heading "Initialize mysql database"
-mysql -h 172.31.43.240 -uroot -p"$1" < /app/schema/backend.sql
+mysql -h 172.31.39.6 -uroot -p"$1" < /app/schema/backend.sql
 STAT $?
 
 # Reload the systemd daemon
